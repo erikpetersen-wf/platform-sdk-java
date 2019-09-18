09e7f3becb574a0a16ba8384fc0e6e1f5a02fb5e
Nate <Nate.Woods@workiva.com>
2019-09-18 12:18:21 -0500

    feat(log): wiring logrus event logger to internal package

--- log/logrus.go
+++ log/logrus.go
@@ -2,8 +2,11 @@ package log
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/sirupsen/logrus"
+
+	"github.com/Workiva/platform/internal"
 )
 
 // New2 ... trying out some logrus ideas (should replace New)
@@ -26,7 +29,33 @@ type logrusHook struct {
 }
 
 func (hook *logrusHook) Levels() []logrus.Level { return logrus.AllLevels }
-func (hook *logrusHook) Fire(log *logrus.Entry) error {
-	panic(`TODO`)
+
+// Internal standard logging levels as defined by RFD-0249
+var levelMap = map[logrus.Level]int{
+	logrus.TraceLevel: 0,
+	logrus.DebugLevel: 0,
+	logrus.InfoLevel:  1,
+	logrus.WarnLevel:  2,
+	logrus.ErrorLevel: 3,
+	logrus.FatalLevel: 4,
+	logrus.PanicLevel: 4,
+}
+
+func (hook *logrusHook) Fire(entry *logrus.Entry) error {
+
+	// normalize level to internal standard (RFD-0249)
+	level, ok := levelMap[entry.Level]
+	if !ok {
+		level = 2 // default to warn :shrug:
+	}
+
+	// normalize data to stringly typed info (it's going to JSON folks!)
+	meta := make(map[string]string, len(entry.Data))
+	for k, v := range entry.Data {
+		meta[k] = fmt.Sprint(v)
+	}
+
+	// Actually log the message to the platform
+	internal.Log(hook.ctx, level, entry.Message, meta)
 	return nil
 }
