262fd85d4ad9aa56ddae2a217f90c8df6560f260
Erik.Petersen <erik.petersen@webfilings.com>
2019-09-23 11:21:41 -0400

    Remove unnecessary extend.

--- libs/java/src/main/java/com/workiva/platform/Result.java
+++ libs/java/src/main/java/com/workiva/platform/Result.java
@@ -2,6 +2,6 @@ package com.workiva.platform;
 
 import io.undertow.util.AttachmentKey;
 
-public class Result extends Object {
+public class Result {
   public static AttachmentKey<Object> KEY = AttachmentKey.create(Object.class);
 }
