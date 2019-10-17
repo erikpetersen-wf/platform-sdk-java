b703bad23ca7b92049d89cda287ca31f585892ef
Erik.Petersen <erik.petersen@webfilings.com>
2019-10-17 13:45:23 -0500

    Add test for frugal health check.

--- libs/java/src/test/java/com/workiva/platform/PlatformTest.java
+++ libs/java/src/test/java/com/workiva/platform/PlatformTest.java
@@ -1,6 +1,15 @@
 package com.workiva.platform;
 
 import com.google.gson.Gson;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
 import io.undertow.util.StatusCodes;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -8,12 +17,15 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.net.ConnectException;
 
 public class PlatformTest {
+
+  static final int frugalPort = 8000;
 
   final CloseableHttpClient httpClient = HttpClients.createDefault();
 
@@ -56,6 +68,57 @@ public class PlatformTest {
     }
     Gson gson = new Gson();
     return gson.toJson(expectedStatus);
+  }
+
+  @BeforeClass
+  public static void setup() {
+    EventLoopGroup bossGroup = new NioEventLoopGroup();
+    EventLoopGroup workerGroup = new NioEventLoopGroup();
+    try {
+      ServerBootstrap b = new ServerBootstrap();
+      b.group(bossGroup, workerGroup)
+          .channel(NioServerSocketChannel.class)
+          .childHandler(
+              new ChannelInitializer<SocketChannel>() {
+                @Override
+                public void initChannel(SocketChannel ch) throws Exception {
+                  // codec+aggregator needed to make a FullHttpRequest.
+                  ch.pipeline().addLast("codec", new HttpServerCodec());
+                  ch.pipeline().addLast("aggregator", new HttpObjectAggregator(512 * 1024 * 10));
+                  ch.pipeline().addLast(new HttpFrugalHealthHandler());
+                }
+              });
+
+      new Thread(
+              () -> {
+                try {
+                  ChannelFuture f = b.bind(frugalPort).sync();
+                  // Blocking
+                  f.channel().closeFuture().sync();
+                } catch (Exception e) {
+                  Assert.fail(
+                      "Error encountered when attempting to standup HTTP-Messaging service");
+                }
+              })
+          .start();
+
+      while (true) {
+        try (final CloseableHttpClient httpClient = HttpClients.createDefault()) {
+          HttpGet httpGet =
+              new HttpGet(
+                  String.format("http://localhost:%d%s", frugalPort, Platform.readinessPath));
+          HttpResponse httpFrugalResp = httpClient.execute(httpGet);
+          int statusCode = httpFrugalResp.getStatusLine().getStatusCode();
+          if (statusCode != 503) {
+            break;
+          }
+          Thread.sleep(30);
+        } catch (Exception e) {
+        }
+      }
+    } catch (Exception e) {
+      Assert.fail("Unable to start test service");
+    }
   }
 
   @Test
@@ -157,5 +220,15 @@ public class PlatformTest {
     String responseStatus = getBodyFromResponse(httpFrugalResp);
     String jsonStatus = createExpectedStatus(false, "uncaught exception");
     Assert.assertEquals(jsonStatus, responseStatus);
+  }
+
+  @Test
+  public void TestCheckFrugalHealth() throws Exception {
+    try (final CloseableHttpClient httpClient = HttpClients.createDefault()) {
+      HttpGet httpGet = new HttpGet("http://localhost:8000/_wk/ready");
+      HttpResponse httpFrugalResp = httpClient.execute(httpGet);
+      int statusCode = httpFrugalResp.getStatusLine().getStatusCode();
+      Assert.assertEquals(statusCode, StatusCodes.OK);
+    }
   }
 }
