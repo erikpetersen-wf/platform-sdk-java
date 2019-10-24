97e3ba8ef194e97ce6e469bd050637a634b0fce7
Erik.Petersen <erik.petersen@webfilings.com>
2019-10-24 16:30:31 -0400

    Add spring-based liveness/readiness checks.

---
+++ libs/java/src/main/java/com/workiva/platform/SpringHealthCheck.java
@@ -0,0 +1,20 @@
+package com.workiva.platform;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@Configuration
+public class SpringHealthCheck {
+
+  @RestController
+  public static class Controller {
+
+    @RequestMapping(value = Platform.readinessPath, method = RequestMethod.GET)
+    public void getReadiness() {}
+
+    @RequestMapping(value = Platform.livenessPath, method = RequestMethod.GET)
+    public void getLiveness() {}
+  }
+}
