61688ea7b137a209de85d05013b13f26cd41f5a7
Nate <Nate.Woods@workiva.com>
2019-11-20 12:13:50 -0600

    feat(status): Update after actually compiling
    mvn -B install -rf :platform-core

--- libs/java/platform-core/src/main/java/com/workiva/platform/core/PlatformCore.java
+++ libs/java/platform-core/src/main/java/com/workiva/platform/core/PlatformCore.java
@@ -1,20 +1,24 @@
 package com.workiva.platform.core;
 
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.concurrent.Callable;
+
 public class PlatformCore {
 
   private boolean isAlive;
-  private Map<String, Callable<boolean>> aliveChecks;
-  private Map<String, Callable<boolean>> readyChecks;
-  private Map<String, Callable<boolean>> statusChecks;
+  private Map<String, Callable<Boolean>> aliveChecks;
+  private Map<String, Callable<Boolean>> readyChecks;
+  private Map<String, Callable<Boolean>> statusChecks;
+
+  public static final String PATH_ALIVE = "/_wk/alive";
+  public static final String PATH_READY = "/_wk/ready";
+  public static final String PATH_STATUS = "/_wk/status";
 
   // old and deprecated
   public static final String livenessPath = PATH_ALIVE;
   public static final String readinessPath = PATH_READY;
   public static final String statusPath = PATH_STATUS;
-
-  public static final String PATH_ALIVE = "/_wk/alive";
-  public static final String PATH_READY = "/_wk/ready";
-  public static final String PATH_STATUS = "/_wk/status";
 
   public enum CheckType {
     // TODO: document the heck out of these guys.
@@ -26,6 +30,7 @@ public class PlatformCore {
   public class HttpResponse {
     public int code;
     public byte[] body;
+
     HttpResponse(int code, byte[] body) {
       this.code = code;
       this.body = body;
@@ -34,9 +39,9 @@ public class PlatformCore {
 
   PlatformCore() {
     isAlive = true;
-    aliveChecks = new Map<String, Callable<boolean>>();
-    readyChecks = new Map<String, Callable<boolean>>();
-    statusChecks = new Map<String, Callable<boolean>>();
+    aliveChecks = new TreeMap<>();
+    readyChecks = new TreeMap<>();
+    statusChecks = new TreeMap<>();
   }
 
   public void shutdown() {
@@ -48,9 +53,11 @@ public class PlatformCore {
       return 418; // I'm as useful as a teapot.
     }
     try {
-      for (Map.Entry<String, Callable<boolean>> entry : aliveChecks.entrySet())
-        if (entry.getValue().call())
+      for (Callable<Boolean> check : aliveChecks.values()) {
+        if (check.call()) {
           return 500;
+        }
+      }
     } catch (Exception e) {
       return 500; // something bad happened
     }
@@ -58,33 +65,55 @@ public class PlatformCore {
   }
 
   public int ready() {
-  try {
-    for (Map.Entry<String, Callable<boolean>> entry : readyChecks.entrySet())
-      if (entry.getValue().call())
-        return 500;
-  } catch (Exception e) {
-    return 500; // something bad happened
-  }
+    try {
+      for (Callable<Boolean> check : readyChecks.values()) {
+        if (check.call()) {
+          return 500;
+        }
+      }
+    } catch (Exception e) {
+      return 500; // something bad happened
+    }
     return 200;
   }
 
   public HttpResponse status() {
+    String response = "\t\t\"meta\": {";
+    for (Map.Entry<String, Callable<Boolean>> entry : statusChecks.entrySet()) {
+      response += "\n\t\t\t\"" + entry.getKey() + "\": \"";
+      try {
+        if (entry.getValue().call()) {
+          response += "PASSED";
+        } else {
+          response += "FAILED";
+        }
+      } catch (Exception e) {
+        response += e.toString();
+      }
+      response += "\",";
+    }
+    response += "\n\t\t}";
     // TODO: statusChecks
-    return new HttpResponse(418, "teapot!".getBytes());
+    return new HttpResponse(418, response.getBytes());
   }
 
-  public void register(String name, Callable<boolean> callback, CheckType type) {
+  public void register(String name, Callable<Boolean> callback, CheckType type) {
     switch (type) {
-    case CheckType.ALIVE:
-      aliveChecks.put(callback);
-    case CheckType.READY:
-      readinessChecks.put(callback);
-    case CheckType.STATUS:
-      statusPath.put(callback);
+      case ALIVE:
+        aliveChecks.put(name, callback);
+        break;
+      case READY:
+        readyChecks.put(name, callback);
+        break;
+      case STATUS:
+        statusChecks.put(name, callback);
+        break;
+      default:
+        System.out.println("unsupported callback!");
     }
   }
 
-  public void register(String name, Callable<boolean> callback) {
+  public void register(String name, Callable<Boolean> callback) {
     register(name, callback, CheckType.STATUS);
   }
 }
