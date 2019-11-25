102cd8f9efe731a609699834c905bb10ccafff87
Erik.Petersen <erik.petersen@webfilings.com>
2019-11-25 09:52:08 -0500

    Simply endpoint handler.

--- libs/java/platform-undertow/src/main/java/com/workiva/platform/undertow/EndpointHandler.java
+++ libs/java/platform-undertow/src/main/java/com/workiva/platform/undertow/EndpointHandler.java
@@ -1,10 +1,8 @@
 package com.workiva.platform.undertow;
 
-import com.google.gson.Gson;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Headers;
-import io.undertow.util.StatusCodes;
 
 import com.workiva.platform.core.PlatformResponse;
 
@@ -18,19 +16,10 @@ public class EndpointHandler implements HttpHandler {
   }
 
   @Override
-  public void handleRequest(HttpServerExchange exchange) {
+  public void handleRequest(HttpServerExchange exchange) throws Exception {
     exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "application/json");
-
-    PlatformResponse result = null;
-    try {
-      result = (PlatformResponse) callable.call();
-    } catch (Exception e) {
-      result = new PlatformResponse(500, e.getMessage());
-
-      exchange.setStatusCode(result.getCode());
-      exchange.getResponseSender().send(result.getBody());
-      return;
-    }
+    
+    PlatformResponse result = (PlatformResponse) callable.call();
 
     exchange.setStatusCode(result.getCode());
     exchange.getResponseSender().send(result.getBody());
