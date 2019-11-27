f834a0ffabd1004e2488fc3fd7cab7319c67c4d1
Erik.Petersen <erik.petersen@webfilings.com>
2019-11-27 15:06:52 -0500

    Remove unneeded variable.

--- libs/java/platform-servlet/src/main/java/com/workiva/platform/servlet/Platform.java
+++ libs/java/platform-servlet/src/main/java/com/workiva/platform/servlet/Platform.java
@@ -7,21 +7,17 @@ import com.workiva.platform.core.PlatformCore;
 
 public class Platform extends PlatformCore {
 
-  private static ServletContextHandler servletContext;
-
   /**
    * Takes a servlet context handler and registers the readiness, liveness, and status endpoints.
    *
    * @param servletContext Context from the Jetty server.
    */
   public void registerEndpoints(ServletContextHandler servletContext) {
-    Platform.servletContext = servletContext;
-
-    Platform.servletContext.addServlet(
-        new ServletHolder(new HealthCheck(this::ready)), PlatformCore.PATH_READY);
-    Platform.servletContext.addServlet(
+    servletContext.addServlet(
+        new ServletHolder(new HealthCheck(this::ready)), PlatformCore.PATH_READY + "/*");
+    servletContext.addServlet(
         new ServletHolder(new HealthCheck(this::alive)), PlatformCore.PATH_ALIVE);
-    Platform.servletContext.addServlet(
+    servletContext.addServlet(
         new ServletHolder(new HealthCheck(this::status)), PlatformCore.PATH_STATUS);
   }
 }
