40671e8a650c92adbe7ebc9cfa40b855bdf3723b
Erik.Petersen <erik.petersen@webfilings.com>
2019-11-27 12:00:39 -0500

    Implement platform class.

--- libs/java/platform-servlet/src/main/java/com/workiva/platform/servlet/Platform.java
+++ libs/java/platform-servlet/src/main/java/com/workiva/platform/servlet/Platform.java
@@ -1,13 +1,22 @@
-package com.workiva.platform;
+package com.workiva.platform.servlet;
+
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
 import com.workiva.platform.core.PlatformCore;
 
 public class Platform extends PlatformCore {
 
-	private ServletContextHandler servletContext;
+  private static ServletContextHandler servletContext;
 
-	public Platform(ServletContextHandler servletContext) {
-		Platform.servletContext = servletContext;
-	}
-	
+  public void registerEndpoints(ServletContextHandler servletContext) {
+    Platform.servletContext = servletContext;
+
+    Platform.servletContext.addServlet(
+        new ServletHolder(new HealthCheck(this::ready)), PlatformCore.PATH_READY);
+    Platform.servletContext.addServlet(
+        new ServletHolder(new HealthCheck(this::alive)), PlatformCore.PATH_ALIVE);
+    Platform.servletContext.addServlet(
+        new ServletHolder(new HealthCheck(this::status)), PlatformCore.PATH_STATUS);
+  }
 }
