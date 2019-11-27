18f9e329ee45c7624f11c57e633bab133a02e1fc
Erik.Petersen <erik.petersen@webfilings.com>
2019-11-27 12:00:30 -0500

    Implement health check class.

--- libs/java/platform-servlet/src/main/java/com/workiva/platform/servlet/HealthCheck.java
+++ libs/java/platform-servlet/src/main/java/com/workiva/platform/servlet/HealthCheck.java
@@ -1,4 +1,10 @@
-package com.workiva.platform;
+package com.workiva.platform.servlet;
+
+import com.workiva.platform.core.PlatformResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.concurrent.Callable;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -6,8 +12,28 @@ import javax.servlet.http.HttpServletResponse;
 
 public class HealthCheck extends HttpServlet {
 
+  private static Callable callable;
+
+  public HealthCheck(Callable c) {
+    HealthCheck.callable = c;
+  }
+
   @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) {
-    response.setStatus(HttpServletResponse.SC_OK);
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    PlatformResponse result = null;
+    try {
+      result = (PlatformResponse) callable.call();
+    } catch (Exception e) {
+      response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+      return;
+    }
+
+    response.setStatus(result.getCode());
+
+    PrintWriter out = response.getWriter();
+    response.setContentType("application/json");
+    response.setCharacterEncoding("UTF-8");
+    out.print(result.getBody());
+    out.flush();
   }
 }
