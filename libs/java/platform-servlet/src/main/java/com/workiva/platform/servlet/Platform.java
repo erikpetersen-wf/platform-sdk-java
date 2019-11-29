99fc07e7fa840890c60272d35f86f07c329468da
Erik.Petersen <erik.petersen@webfilings.com>
2019-11-29 15:54:01 -0500

    Refactor existing method.

--- libs/java/platform-servlet/src/main/java/com/workiva/platform/servlet/Platform.java
+++ libs/java/platform-servlet/src/main/java/com/workiva/platform/servlet/Platform.java
@@ -5,21 +5,28 @@ import org.eclipse.jetty.servlet.ServletHolder;
 
 import com.workiva.platform.core.PlatformCore;
 
+import java.util.List;
+
 public class Platform extends PlatformCore {
 
   static final String PATH_PREFIX = "/_wk";
+
   /**
-   * Takes a servlet context handler and registers the readiness, liveness, and status endpoints.
+   * Takes a servlet context handler and registers health check endpoints.
    *
    * @param servletContext Context from the Jetty server.
    */
   public void registerEndpoints(ServletContextHandler servletContext) {
+    List<String> endpointPaths = Utils.stripPrefix(servletContext.getContextPath());
+
     servletContext.addServlet(
-        new ServletHolder(new HealthCheck(this::ready)), PlatformCore.PATH_READY + "/*");
+        new ServletHolder(new HealthCheck(this::ready)), endpointPaths.get(0));
     servletContext.addServlet(
-        new ServletHolder(new HealthCheck(this::alive)), PlatformCore.PATH_ALIVE);
+        new ServletHolder(new HealthCheck(this::alive)), endpointPaths.get(1));
     servletContext.addServlet(
-        new ServletHolder(new HealthCheck(this::status)), PlatformCore.PATH_STATUS);
+        new ServletHolder(new HealthCheck(this::status)), endpointPaths.get(2));
+  }
+
   /**
    * Creates a servlet context and registers health check endpoints.
    *
