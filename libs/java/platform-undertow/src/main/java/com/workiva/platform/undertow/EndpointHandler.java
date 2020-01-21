6f2447dfdfea26ae8a1239df4e1d2cdcf0e20497
Erik.Petersen <erik.petersen@webfilings.com>
2020-01-21 13:55:42 -0500

    Set forwarded IP.

--- libs/java/platform-undertow/src/main/java/com/workiva/platform/undertow/EndpointHandler.java
+++ libs/java/platform-undertow/src/main/java/com/workiva/platform/undertow/EndpointHandler.java
@@ -4,7 +4,9 @@ import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Headers;
 
+import com.workiva.platform.core.PlatformCore;
 import com.workiva.platform.core.PlatformResponse;
+import com.workiva.platform.core.StatusHandler;
 
 import java.util.concurrent.Callable;
 
@@ -18,6 +20,11 @@ public class EndpointHandler implements HttpHandler {
   @Override
   public void handleRequest(HttpServerExchange exchange) throws Exception {
     exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "application/json");
+
+    if (callable.getClass().equals(StatusHandler.class)) {
+      String forwardedFor = exchange.getRequestHeaders().getFirst(PlatformCore.FORWARDED_FOR);
+      ((StatusHandler) callable).setForwardedFor(forwardedFor);
+    }
 
     PlatformResponse result = (PlatformResponse) callable.call();
 
