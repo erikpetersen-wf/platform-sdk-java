0514527259243b8e6ccf1cc32a182b428113ce20
Nate <Nate.Woods@workiva.com>
2020-02-28 12:49:37 -0600

    feat(rdb): Adding initial RDB implementation

---
+++ rdb/connect.go
@@ -0,0 +1,64 @@
+package db
+
+import (
+	"database/sql"
+	"errors"
+	"os"
+	"time"
+
+	// Forcing mysql driver and new-relic integration
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/newrelic/go-agent/_integrations/nrmysql"
+)
+
+// used for unit tests (gross, I know)
+var driverName = "nrmysql"
+
+// Connect pulls information from your environment and connects to a database.
+func Connect() (*sql.DB, error) {
+	if getenv("RDS_HOST") == "" {
+		return nil, errors.New("rdb: No database resource provisioned")
+	}
+	db, err := sql.Open(driverName, dsn())
+	if err != nil {
+		return nil, err
+	}
+	// By default, this is 0 which sets max number of open conns to infinity.
+	// We need to monitor this to ensure we don't overwhelm the container if
+	// we open too many TCP connections simultaneously. Use zero for "infinity"
+	db.SetMaxOpenConns(0)
+	// we have to watch idle conns when using aurora as described
+	// here: https://github.com/go-sql-driver/mysql/issues/363
+	db.SetMaxIdleConns(100)
+	// by default, MySQL has a wait_timeout of 8 hours.  Just to be
+	// safe, we set it to 30 minutes.  If we don't set this, queries
+	// to mysql will fail with an ErrBadConn error.
+	db.SetConnMaxLifetime(30 * time.Minute)
+
+	// Verify connection to the database
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+// used for unit tests (gross, I know)
+var getenv = os.Getenv
+
+func dsn() string {
+	name := getenv("WORKIVA_SERVICE_NAME")
+	user := getenv("RDS_USER")
+	host := getenv("RDS_HOST")
+	pass := getenv("RDS_PASSWORD")
+	// Config stolen from skaardb (TODO: document each option)
+	// https://github.com/Workiva/skaardb/search?q=dsn_params&unscoped_q=dsn_params
+	// https://github.com/Workiva/skaardb/blob/master/cerberus/config/persister_config.go
+	params := "parseTime=true&"
+	params += "timeout=90s&"
+	params += "writeTimeout=90s&"
+	params += "readTimeout=90s&"
+	params += "tls=skip-verify&"
+	params += "maxAllowedPacket=1000000000&"
+	params += "rejectReadOnly=true"
+	return user + ":" + pass + "@tcp(" + host + ":3306)/" + name + "?" + params
+}
---
+++ rdb/connect_test.go
@@ -0,0 +1,92 @@
+package db
+
+import (
+	"database/sql"
+	"database/sql/driver"
+	"errors"
+	"os"
+	"testing"
+)
+
+func fakeENV(tb testing.TB) func() {
+	getenv = func(name string) string {
+		switch name {
+		case "WORKIVA_SERVICE_NAME":
+			return "service"
+		case "RDS_USER":
+			return "user"
+		case "RDS_HOST":
+			return "host"
+		case "RDS_PASSWORD":
+			return "pass"
+		default:
+			tb.Fatalf("unknown getenv call: %v", name)
+			return "wtf-mate"
+		}
+	}
+	return func() {
+		getenv = os.Getenv
+	}
+}
+
+func TestDSN(t *testing.T) {
+	defer fakeENV(t)()
+	str := dsn()
+	exp := "user:pass@tcp(host:3306)/service?parseTime=true&timeout=90s&writeTimeout=90s&"
+	exp += "readTimeout=90s&tls=skip-verify&maxAllowedPacket=1000000000&rejectReadOnly=true"
+	if str != exp {
+		t.Errorf("Did not retrieve expected DSN\nGOT : %q\nWANT: %q", str, exp)
+	}
+}
+
+type fakeSQL struct {
+	openErr error
+	closed  bool
+}
+
+func (d *fakeSQL) Open(name string) (driver.Conn, error)     { return d, d.openErr }
+func (d *fakeSQL) Prepare(query string) (driver.Stmt, error) { return nil, nil }
+func (d *fakeSQL) Begin() (driver.Tx, error)                 { return nil, nil }
+
+// func (d *fakeSQL) Ping(ctx context.Context) error            { return d.pingErr }
+func (d *fakeSQL) Close() error {
+	d.closed = true
+	return nil
+}
+
+func TestConnect(t *testing.T) {
+	defer fakeENV(t)()
+	defer func() {
+		driverName = "nrmysql"
+	}()
+	driverName = "fake"
+	d := &fakeSQL{}
+	sql.Register("fake", d)
+
+	// Case 0: noop everything
+	_, err := Connect()
+	if err != nil {
+		t.Fatalf("Couldn't connect: %v", err)
+	}
+
+	// Case 1: ping failure
+	d.openErr = errors.New("bad ping")
+	_, err = Connect()
+	if err == nil || err.Error() != "bad ping" {
+		t.Fatalf("Unexpected ping error: %v", err)
+	}
+
+	// Case 2: open failure
+	driverName = "other"
+	_, err = Connect()
+	if err == nil || err.Error() != "sql: unknown driver \"other\" (forgotten import?)" {
+		t.Fatalf("Unexpected open error: %v", err)
+	}
+}
+
+func TestBadConnect(t *testing.T) {
+	_, err := Connect()
+	if err == nil || err.Error() != "rdb: No database resource provisioned" {
+		t.Fatalf("Unexpected connect error: %v", err)
+	}
+}
