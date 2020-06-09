39e3dcc812dc064795e3061d956e10905b9187c9
Brian Shannan <brian.shannan@workiva.com>
2020-06-09 12:42:23 -0500

    change MaxIdleConnsPerHost to 100

--- platform.go
+++ platform.go
@@ -73,6 +73,7 @@ func NewHTTPTransport() *http.Transport {
 		DialContext:           dialer.DialContext,
 		ForceAttemptHTTP2:     true, // added in go 1.13, if your build breaks here.. update
 		MaxIdleConns:          100,
+		MaxIdleConnsPerHost:   100,
 		IdleConnTimeout:       90 * time.Second,
 		TLSHandshakeTimeout:   10 * time.Second,
 		ExpectContinueTimeout: 1 * time.Second,
