5ccd78778aeb5d17829f872ac8a0cf4f91e72770
Brian Shannan <brian.shannan@workiva.com>
2020-06-09 12:41:15 -0500

    function to compare default and custom http transports

--- platform.go
+++ platform.go
@@ -2,10 +2,12 @@ package platform
 
 import (
 	"context"
+	"fmt"
 	"net"
 	"net/http"
 	"time"
 
+	"github.com/Sirupsen/logrus"
 	"github.com/Workiva/platform/internal"
 )
 
@@ -50,12 +52,23 @@ func WithPort(ctx context.Context, port int) context.Context {
 // using req.WithContext(ctx): https://golang.org/pkg/net/http/#Request.WithContext
 func NewHTTPClient() *http.Client {
 	// borrowed from net/http::DefaultTransport to ensure we don't have shared globals
+	return &http.Client{
+		Transport: NewHTTPTransport(),
+
+		// Default timeout for all requests going through this client.
+		Timeout: 60 * time.Second,
+	}
+}
+
+func NewHTTPTransport() *http.Transport {
+	// borrowed from net/http::DefaultTransport to ensure we don't have shared globals
 	dialer := &net.Dialer{
 		Timeout:   30 * time.Second,
 		KeepAlive: 30 * time.Second,
 		DualStack: true,
 	}
-	transport := &http.Transport{
+
+	return &http.Transport{
 		Proxy:                 http.ProxyFromEnvironment,
 		DialContext:           dialer.DialContext,
 		ForceAttemptHTTP2:     true, // added in go 1.13, if your build breaks here.. update
@@ -64,10 +77,62 @@ func NewHTTPClient() *http.Client {
 		TLSHandshakeTimeout:   10 * time.Second,
 		ExpectContinueTimeout: 1 * time.Second,
 	}
-	return &http.Client{
-		Transport: transport,
+}
 
-		// Default timeout for all requests going through this client.
-		Timeout: 60 * time.Second,
+// Logs any differences between the current default transport and the transport
+// the repo creates. Returns true if any changes were detected, false otherwise.
+func CheckTransportDifferences() bool {
+	defaultTransport := *http.DefaultTransport.(*http.Transport)
+
+	// TODO get from whatever function
+	customTransport := NewHTTPTransport()
+
+	logFields := logrus.Fields{}
+
+	if defaultTransport.ForceAttemptHTTP2 != customTransport.ForceAttemptHTTP2 {
+		logFields["force_attempt_http2"] = fmt.Sprintf("old: %v, new %v", defaultTransport.ForceAttemptHTTP2, customTransport.ForceAttemptHTTP2)
 	}
+	if defaultTransport.ReadBufferSize != customTransport.ReadBufferSize {
+		logFields["read_buffer_size"] = fmt.Sprintf("old: %v, new %v", defaultTransport.ReadBufferSize, customTransport.ReadBufferSize)
+	}
+	if defaultTransport.WriteBufferSize != customTransport.WriteBufferSize {
+		logFields["write_buffer_size"] = fmt.Sprintf("old: %v, new %v", defaultTransport.WriteBufferSize, customTransport.WriteBufferSize)
+	}
+	if defaultTransport.MaxResponseHeaderBytes != customTransport.MaxResponseHeaderBytes {
+		logFields["max_response_header_bytes"] = fmt.Sprintf("old: %v, new %v", defaultTransport.MaxResponseHeaderBytes, customTransport.MaxResponseHeaderBytes)
+	}
+	if defaultTransport.ExpectContinueTimeout != customTransport.ExpectContinueTimeout {
+		logFields["expect_continue_timeout"] = fmt.Sprintf("old: %v, new %v", defaultTransport.ExpectContinueTimeout, customTransport.ExpectContinueTimeout)
+	}
+	if defaultTransport.ResponseHeaderTimeout != customTransport.ResponseHeaderTimeout {
+		logFields["response_header_timeout"] = fmt.Sprintf("old: %v, new %v", defaultTransport.ResponseHeaderTimeout, customTransport.ResponseHeaderTimeout)
+	}
+	if defaultTransport.IdleConnTimeout != customTransport.IdleConnTimeout {
+		logFields["idle_conn_timeout"] = fmt.Sprintf("old: %v, new %v", defaultTransport.IdleConnTimeout, customTransport.IdleConnTimeout)
+	}
+	if defaultTransport.MaxConnsPerHost != customTransport.MaxConnsPerHost {
+		logFields["max_conns_per_host"] = fmt.Sprintf("old: %v, new %v", defaultTransport.MaxConnsPerHost, customTransport.MaxConnsPerHost)
+	}
+	if defaultTransport.MaxIdleConnsPerHost != customTransport.MaxIdleConnsPerHost {
+		logFields["max_idle_conns_per_host"] = fmt.Sprintf("old: %v, new %v", defaultTransport.MaxIdleConnsPerHost, customTransport.MaxIdleConnsPerHost)
+	}
+	if defaultTransport.MaxIdleConns != customTransport.MaxIdleConns {
+		logFields["max_idle_conns"] = fmt.Sprintf("old: %v, new %v", defaultTransport.MaxIdleConns, customTransport.MaxIdleConns)
+	}
+	if defaultTransport.DisableCompression != customTransport.DisableCompression {
+		logFields["disable_compression"] = fmt.Sprintf("old: %v, new %v", defaultTransport.DisableCompression, customTransport.DisableCompression)
+	}
+	if defaultTransport.DisableKeepAlives != customTransport.DisableKeepAlives {
+		logFields["disable_keep_alives"] = fmt.Sprintf("old: %v, new %v", defaultTransport.DisableKeepAlives, customTransport.DisableKeepAlives)
+	}
+	if defaultTransport.TLSHandshakeTimeout != customTransport.TLSHandshakeTimeout {
+		logFields["tls_handshake_timeout"] = fmt.Sprintf("old: %v, new %v", defaultTransport.TLSHandshakeTimeout, customTransport.TLSHandshakeTimeout)
+	}
+
+	if len(logFields) > 0 {
+		logrus.WithFields(logFields).Warn("changes from default client to custom client")
+		return true
+	}
+
+	return false
 }
